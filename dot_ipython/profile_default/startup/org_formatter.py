9371c53ac37feb7c1efc7fa85124e35188b4472c
Brendan Murphy <brendan.murphy@bristol.ac.uk>
2025-10-24 13:23:58 +0100

    Don't use OrgFormatter on strings

--- dot_ipython/profile_default/startup/org_formatter.py
+++ dot_ipython/profile_default/startup/org_formatter.py
@@ -4,6 +4,10 @@ from tabulate import tabulate
 
 class OrgFormatter(IPython.core.formatters.BaseFormatter):
     def __call__(self, obj):
+        # don't convert strings into tables
+        if isinstance(obj, (str, bytes)):
+            return None
+
         try:
             return tabulate(obj, headers='keys', tablefmt='orgtbl', showindex='always')
         except:
